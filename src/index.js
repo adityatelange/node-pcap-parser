4175b800dfbfd2838771c1f0f3327f4b5960ee01
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-01-24 23:52:05 +0530

    Comment out logs

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ async function PcapParser(file) {
     let pcap = await readFile(file);
 
     pcap = pcap.match(/.{1,2}/g)
-    console.log(pcap)
+    // console.log(pcap)
 
     var current = 0     // specifies the current location while parsing the hex obj
 
@@ -23,7 +23,7 @@ async function PcapParser(file) {
     const lenfileHeader = 24    // according to specs, fileHeader is of 24 bytes
     var fileHeader = pcap.slice(current, lenfileHeader);        // take the fileHeader out
     current += lenfileHeader        // update the current location
-    console.log("fileHeader", fileHeader);
+    // console.log("fileHeader", fileHeader);
 
     // Packets
     var pkid = 0
@@ -33,16 +33,16 @@ async function PcapParser(file) {
         const lenpacketRecord = 16      // according to specs, packetRecord is of 16 bytes
         let packetRecord = pcap.slice(current, current + lenpacketRecord);      // take the packetRecord out
         current += lenpacketRecord      // update the current location
-        console.log("packetRecord", pkid, packetRecord);
+        // console.log("packetRecord", pkid, packetRecord);
 
         let cpl = packetRecord.slice(8, 11).reverse().join('')      // extract CPL from packetRecord as a wholw hex
         cpl = parseInt(cpl, 16)     // convert hex to decimal
-        console.log("Captured Packet Length", pkid, cpl);
+        // console.log("Captured Packet Length", pkid, cpl);
 
         const lenpacketData = cpl       // consider packetData lenth to be CPL between CPL and OPL
         let packetData = pcap.slice(current, current + lenpacketData);      // take the packetData out
         current += lenpacketData        // update the current location
-        console.log("packetData", pkid, packetData);
+        // console.log("packetData", pkid, packetData);
 
         packets.push({ packetRecord, packetData })
         pkid += 1
