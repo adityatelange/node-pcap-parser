ba48721678e125b23235829f3336589cb0179b34
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-01-25 00:26:17 +0530

    Retuns Humanized version of hex data

--- src/index.js
+++ src/index.js
@@ -11,6 +11,29 @@ async function readFile(path) {
     });
 }
 
+function fileHeaderHumanize(input) {
+    return {
+        magicnumber: parseInt(input.slice(0, 4).reverse().join(''), 16),
+        majorversion: parseInt(input.slice(4, 6).reverse().join(''), 16),
+        minorversion: parseInt(input.slice(6, 8).reverse().join(''), 16),
+        reserved1: parseInt(input.slice(8, 12).reverse().join(''), 16),
+        reserved2: parseInt(input.slice(12, 16).reverse().join(''), 16),
+        snaplen: parseInt(input.slice(16, 20).reverse().join(''), 16),
+        linktype: parseInt(input.slice(20, 24).reverse().join(''), 16)
+    }
+}
+
+function packetRecordHumanize(packetRecord, packet) {
+    return {
+        timestampS: parseInt(packetRecord.slice(0, 4).reverse().join(''), 16),
+        timestampMs: parseInt(packetRecord.slice(4, 8).reverse().join(''), 16),
+        cpl: parseInt(packetRecord.slice(8, 12).reverse().join(''), 16),
+        opl: parseInt(packetRecord.slice(12, 16).reverse().join(''), 16),
+        packet: packet
+    }
+}
+
+
 async function PcapParser(file) {
     let pcap = await readFile(file);                                            // read file from given location
 
@@ -22,6 +45,7 @@ async function PcapParser(file) {
     // FileHeader
     const lenfileHeader = 24                                                    // according to specs, fileHeader is of 24 bytes
     var fileHeader = pcap.slice(current, lenfileHeader);                        // take the fileHeader out
+    fileHeader = fileHeaderHumanize(fileHeader)
     current += lenfileHeader                                                    // update the current location
     // console.log("fileHeader", fileHeader);
 
@@ -44,7 +68,7 @@ async function PcapParser(file) {
         current += lenpacketData                                                // update the current location
         // console.log("packetData", pkid, packetData);
 
-        packets.push({ packetRecord, packetData })
+        packets.push(packetRecordHumanize(packetRecord, packetData))
         // pkid += 1
     }
 
