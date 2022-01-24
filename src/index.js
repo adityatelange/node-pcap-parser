37b582abbd511baf483d794804e87eab85504fd3
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-01-24 23:44:48 +0530

    Use readFile async funct to return data from file
    https://stackoverflow.com/a/57755575

--- src/index.js
+++ src/index.js
@@ -1,43 +1,49 @@
 fs = require('fs');
 
-
-function PcapParser(file) {
-    fs.readFile(file, 'hex', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        let pcap = data
-        pcap = pcap.match(/.{1,2}/g)
-        console.log(pcap)
-
-        var current = 0     // specifies the current location while parsing the hex obj
-
-        // FileHeader
-        const lenfileHeader = 24    // according to specs, fileHeader is of 24 bytes
-        var fileHeader = pcap.slice(current, lenfileHeader);        // take the fileHeader out
-        current += lenfileHeader        // update the current location
-        console.log("fileHeader", fileHeader);
-
-        var pkid = 0
-        while (current < pcap.length) {
-            // packetRecord
-            const lenpacketRecord = 16      // according to specs, packetRecord is of 16 bytes
-            let packetRecord = pcap.slice(current, current + lenpacketRecord);      // take the packetRecord out
-            current += lenpacketRecord      // update the current location
-            console.log("packetRecord", pkid, packetRecord);
-
-            let cpl = packetRecord.slice(8, 11).reverse().join('')      // extract CPL from packetRecord as a wholw hex
-            cpl = parseInt(cpl, 16)     // convert hex to decimal
-            console.log("Captured Packet Length", pkid, cpl);
-
-            const lenpacketData = cpl       // consider packetData lenth to be CPL between CPL and OPL
-            let packetData = pcap.slice(current, current + lenpacketData);      // take the packetData out
-            current += lenpacketData        // update the current location
-            console.log("packetData", pkid, packetData);
-
-            pkid += 1
-        }
+async function readFile(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'hex', function (err, data) {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
     });
+}
+
+async function PcapParser(file) {
+    let pcap = await readFile(file);
+
+    pcap = pcap.match(/.{1,2}/g)
+    console.log(pcap)
+
+    var current = 0     // specifies the current location while parsing the hex obj
+
+    // FileHeader
+    const lenfileHeader = 24    // according to specs, fileHeader is of 24 bytes
+    var fileHeader = pcap.slice(current, lenfileHeader);        // take the fileHeader out
+    current += lenfileHeader        // update the current location
+    console.log("fileHeader", fileHeader);
+
+    var pkid = 0
+    while (current < pcap.length) {
+        // packetRecord
+        const lenpacketRecord = 16      // according to specs, packetRecord is of 16 bytes
+        let packetRecord = pcap.slice(current, current + lenpacketRecord);      // take the packetRecord out
+        current += lenpacketRecord      // update the current location
+        console.log("packetRecord", pkid, packetRecord);
+
+        let cpl = packetRecord.slice(8, 11).reverse().join('')      // extract CPL from packetRecord as a wholw hex
+        cpl = parseInt(cpl, 16)     // convert hex to decimal
+        console.log("Captured Packet Length", pkid, cpl);
+
+        const lenpacketData = cpl       // consider packetData lenth to be CPL between CPL and OPL
+        let packetData = pcap.slice(current, current + lenpacketData);      // take the packetData out
+        current += lenpacketData        // update the current location
+        console.log("packetData", pkid, packetData);
+
+        pkid += 1
+    }
 }
 
 module.exports.PcapParser = PcapParser
