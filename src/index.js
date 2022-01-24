d02acf7e348340d3bd7c20ab025cb9761acceec0
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2022-01-25 00:00:27 +0530

    Refactor comments

--- src/index.js
+++ src/index.js
@@ -12,40 +12,40 @@ async function readFile(path) {
 }
 
 async function PcapParser(file) {
-    let pcap = await readFile(file);
+    let pcap = await readFile(file);                                            // read file from given location
 
     pcap = pcap.match(/.{1,2}/g)
     // console.log(pcap)
 
-    var current = 0     // specifies the current location while parsing the hex obj
+    var current = 0                                                             // specifies the current location while parsing the hex obj
 
     // FileHeader
-    const lenfileHeader = 24    // according to specs, fileHeader is of 24 bytes
-    var fileHeader = pcap.slice(current, lenfileHeader);        // take the fileHeader out
-    current += lenfileHeader        // update the current location
+    const lenfileHeader = 24                                                    // according to specs, fileHeader is of 24 bytes
+    var fileHeader = pcap.slice(current, lenfileHeader);                        // take the fileHeader out
+    current += lenfileHeader                                                    // update the current location
     // console.log("fileHeader", fileHeader);
 
     // Packets
-    var pkid = 0
+    // var pkid = 0
     let packets = []
     while (current < pcap.length) {
         // packetRecord
-        const lenpacketRecord = 16      // according to specs, packetRecord is of 16 bytes
+        const lenpacketRecord = 16                                              // according to specs, packetRecord is of 16 bytes
         let packetRecord = pcap.slice(current, current + lenpacketRecord);      // take the packetRecord out
-        current += lenpacketRecord      // update the current location
+        current += lenpacketRecord                                              // update the current location
         // console.log("packetRecord", pkid, packetRecord);
 
-        let cpl = packetRecord.slice(8, 11).reverse().join('')      // extract CPL from packetRecord as a wholw hex
-        cpl = parseInt(cpl, 16)     // convert hex to decimal
+        let cpl = packetRecord.slice(8, 11).reverse().join('')                  // extract CPL from packetRecord as a whole hex
+        cpl = parseInt(cpl, 16)                                                 // convert hex to decimal
         // console.log("Captured Packet Length", pkid, cpl);
 
-        const lenpacketData = cpl       // consider packetData lenth to be CPL between CPL and OPL
-        let packetData = pcap.slice(current, current + lenpacketData);      // take the packetData out
-        current += lenpacketData        // update the current location
+        const lenpacketData = cpl                                               // consider packetData lenth to be CPL between CPL and OPL
+        let packetData = pcap.slice(current, current + lenpacketData);          // take the packetData out
+        current += lenpacketData                                                // update the current location
         // console.log("packetData", pkid, packetData);
 
         packets.push({ packetRecord, packetData })
-        pkid += 1
+        // pkid += 1
     }
 
     return { fileHeader, packets }
